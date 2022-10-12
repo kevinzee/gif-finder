ba5d8ec4a923051c6033ac207f7bcec297ce1747
kevinzee <103382096+kevinzee@users.noreply.github.com>
2022-10-12 11:14:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 console.log("Script running");
 
-// Helper function - gets a random integer up to (but not including) the maximum
 const getRandomInt = (max) => {
   return Math.floor(Math.random() * Math.floor(max));
 };
 
-// Capture the three major foci of user interaction in variables.
 const submitButton = document.querySelector("#submit");
 const queryField = document.querySelector("#search");
 const imageHolderDiv = document.querySelector("#imageholder");
 
-// Log the elements to confirm that the querySelectors worked.
 console.log(submitButton);
 console.log(queryField);
 console.log(imageHolderDiv);
