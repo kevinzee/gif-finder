bce29008f9af130ee573f5fb7db6b18a38e658fa
kevin zielinski <103382096+kevinzee@users.noreply.github.com>
2022-10-12 15:10:59 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+console.log("Script running");
+
+// Helper function - gets a random integer up to (but not including) the maximum
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * Math.floor(max));
+};
+
+// Capture the three major foci of user interaction in variables.
+const submitButton = document.querySelector("#submit");
+const queryField = document.querySelector("#search");
+const imageHolderDiv = document.querySelector("#imageholder");
+
+// Log the elements to confirm that the querySelectors worked.
+console.log(submitButton);
+console.log(queryField);
+console.log(imageHolderDiv);
+
+submitButton.addEventListener("click", async (e) => {
+  let myKey = "Si7Th7BtVQbBTMD5AYyGFYAEXQfk8ApQ";
+  let topic = queryField.value;
+
+  const response = await fetch("https://api.giphy.com/v1/gifs/search?api_key=Si7Th7BtVQbBTMD5AYyGFYAEXQfk8ApQ&q=" + topic );
+
+  const body = await response.json();
+  const randomIndex = getRandomInt(10)
+  const gifObject = body.data[randomIndex];
+  const image = gifObject.images.downsized_large;
+  const imageURL = image.url;
+  console.log(imageURL);
+  imageHolderDiv.innerHTML = `<img src='${imageURL}' alt='Searched gif goes here.'/>` + imageHolderDiv.innerHTML;
+});
+
+queryField.addEventListener("change", async (e) => {
+  console.log("change happened!");
+  let myKey = "Si7Th7BtVQbBTMD5AYyGFYAEXQfk8ApQ";
+  let topic = queryField.value;
+
+  const response = await fetch("https://api.giphy.com/v1/gifs/search?api_key=Si7Th7BtVQbBTMD5AYyGFYAEXQfk8ApQ&q=" + topic );
+
+  const body = await response.json();
+  const randomIndex = getRandomInt(10)
+  const gifObject = body.data[randomIndex];
+  const image = gifObject.images.downsized_large;
+  const imageURL = image.url;
+  console.log(imageURL);
+  imageHolderDiv.innerHTML = `<img src='${imageURL}' alt='Searched gif goes here.'/>` + imageHolderDiv.innerHTML;
+});
